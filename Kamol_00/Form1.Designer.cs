f083ad82f47ba7f92560abe0c473c00b879ef23b
Kamol <kamolmuattar@gmail.com>
2019-12-07 11:13:32 +0300

    Initial commit

---
+++ Kamol_00/Form1.Designer.cs
@@ -0,0 +1,135 @@
+﻿namespace Kamol_00
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Palatino Linotype", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.label1.Location = new System.Drawing.Point(35, 18);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 87);
+            this.label1.TabIndex = 5;
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.Red;
+            this.button2.Font = new System.Drawing.Font("Arial", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button2.Location = new System.Drawing.Point(382, 187);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(112, 36);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Noto\'g\'ri\r\n";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.MediumSpringGreen;
+            this.button1.Font = new System.Drawing.Font("Arial", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.Location = new System.Drawing.Point(203, 187);
+            this.button1.Name = "button1";
+            this.button1.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.button1.Size = new System.Drawing.Size(111, 36);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "To\'g\'ri";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Arial Black", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(3, 93);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 68);
+            this.label2.TabIndex = 6;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Harlow Solid Italic", 14.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(325, 226);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(158, 24);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Eshpo\'latov Kamol ";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Arial Narrow", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label4.Location = new System.Drawing.Point(258, 164);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(0, 20);
+            this.label4.TabIndex = 8;
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.ClientSize = new System.Drawing.Size(495, 251);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Oddiy Matematika";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ Kamol_00/Form1.cs
@@ -0,0 +1,293 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Kamol_00
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+        bool t = false, t1 = false;
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            label4.Text = "Xato qilish imkomiyatlar soni = 3";
+            Random son = new Random();
+            double a, b, x, y, z;int c;
+            a = son.Next(1, 100);
+            b = son.Next(1, 100);
+            c = son.Next(1, 5);
+            switch (c)
+            {
+                case 1:
+                    {
+                        x = a + b;;
+                        c = son.Next(2);
+                        if (c == 1)
+                        {
+                            z = son.Next(1, 100);t1 = true;
+                            label1.Text = a.ToString() + " + " + b.ToString() + " = " + z.ToString();
+                        }
+                        else 
+                        {
+                            t = true;
+                            label1.Text = a.ToString() + " + " + b.ToString() + " = " + x.ToString();
+                        }
+                    }; break;
+                case 2:
+                    {
+                        x = a - b;
+                        c = son.Next(2);
+                        if (c == 1)
+                        {
+                            z = son.Next(1, 100); t1 = true;
+                            label1.Text = a.ToString() + " - " + b.ToString() + " = " + z.ToString();
+                        }
+                        else
+                        {
+                           t = true;
+                            label1.Text = a.ToString() + " - " + b.ToString() + " = " + x.ToString();
+                        }
+                      
+                    }; break;
+                case 3:
+                    {
+                        x = a / b;
+                        c = son.Next(2);
+                        if (c == 1)
+                        {
+                            z = son.Next(1, 100); t1 = true;
+                            label1.Text = a.ToString() + " / " + b.ToString() + " = " + z.ToString();
+                        }
+                        else 
+                        {
+                             t = true;
+                            label1.Text = a.ToString() + " / " + b.ToString() + " = " + x.ToString();
+                        }
+                    }; break;
+                case 4:
+                    {
+                        x = a * b;
+                        c = son.Next(2);
+                        if (c == 1)
+                        {
+                            z = son.Next(1, 100); t1 = true;
+                            label1.Text = a.ToString() + " * " + b.ToString() + " = " + z.ToString();
+                        }
+                        else 
+                        {
+                             t = true;
+                            label1.Text = a.ToString() + " * " + b.ToString() + " = " + x.ToString();
+                        }
+                    }; break;
+            }
+        }
+
+
+        int javob = 0, xato = 0, xx = 3;
+ 
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (t1 && !t)
+            {
+                javob++;
+                label2.Text = " \r " + javob.ToString();t1 = false;t = false;   
+            }
+            else
+            {
+                xato++;
+                if (xato == 3) {
+                    xx--;
+                    label4.Text = "Xato qilish imkomiyatlar soni = " + xx.ToString();
+                    MessageBox.Show("Siz uchta xato qildingiz ","Oddiy Matematika");
+                    Close();
+                }
+                else
+                {
+                    xx--;
+                    label4.Text = "Xato qilish imkomiyatlar soni = " + xx.ToString();
+                }
+                t = false;t1 = false;
+            }
+            Random son = new Random();
+            double a, b, x, y, z;
+            int c;
+            a = son.Next(1, 100);
+            b = son.Next(1, 100);
+            c = son.Next(1, 5);
+            switch (c)
+            {
+                case 1:
+                    {
+                        x = a + b;
+                        c = son.Next(2);
+                        if (c == 0)
+                        {
+                            z = son.Next(1, 100); t1 = true;
+                            label1.Text = a.ToString() + " + " + b.ToString() + " = " + z.ToString();
+                        }
+                        else 
+                        {
+                             t = true;
+                            label1.Text = a.ToString() + " + " + b.ToString() + " = " + x.ToString();
+                        }
+                    }; break;
+                case 2:
+                    {
+                        x = a - b;
+                        c = son.Next(2);
+                        if (c == 0)
+                        {
+                            z = son.Next(1, 100); t1 = true;
+                            label1.Text = a.ToString() + " - " + b.ToString() + " = " + z.ToString();
+                        }
+                        else 
+                        {
+                             t = true;
+                            label1.Text = a.ToString() + " - " + b.ToString() + " = " + x.ToString();
+                        }
+                    }; break;
+                case 3:
+                    {
+                        x = a / b;
+                        c = son.Next(2);
+                        if (c == 1)
+                        {
+                            z = son.Next(1, 100); t1 = true;
+                            label1.Text = a.ToString() + " / " + b.ToString() + " = " + z.ToString();
+                        }
+                        else 
+                        {
+                             t = true;
+                            label1.Text = a.ToString() + " / " + b.ToString() + " = " + x.ToString();
+                        }
+                    }; break;
+                case 4:
+                    {
+                        x = a * b;
+                        c = son.Next(2);
+                        if (c == 1)
+                        {
+                            z = son.Next(1, 100); t1 = true;
+                            label1.Text = a.ToString() + " * " + b.ToString() + " = " + z.ToString();
+                        }
+                        else 
+                        {
+                             t = true;
+                            label1.Text = a.ToString() + " * " + b.ToString() + " = " + x.ToString();
+                        }
+                    }; break;
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (t && !t1) {
+                javob++;
+            label2.Text=" \r "+javob.ToString();
+                t = false;t1 = false;
+            }
+            else
+            {
+                xato++;
+                if (xato == 3)
+                {
+                    xx--;
+                    label4.Text = "Xato qilish imkomiyatlar soni = " + xx.ToString();
+                    MessageBox.Show("Siz uchta xtao qildingiz ", "Oddiy Matematika");
+                    Close();
+                }
+                else
+                {
+                    xx--;
+                    label4.Text = "Xato qilish imkomiyatlar soni = " + xx.ToString();
+                }
+
+                t = false; t1 = false;
+            }
+            Random son = new Random();
+            int a, b, c, x, y, z;
+            a = son.Next(1, 100);
+            b = son.Next(1, 100);
+            c = son.Next(1, 5);
+            switch (c)
+            {
+                case 1: {
+                        x = a + b;
+                        c = son.Next(2);
+                        if (c == 1)
+                        {
+                            z = son.Next(1, 100); t1 = true;
+                            label1.Text = a.ToString() + " + " + b.ToString() + " = " + z.ToString();
+                        }
+                        else 
+                        {
+                            t = true;
+                            label1.Text = a.ToString() + " + " + b.ToString() + " = " + x.ToString();
+                        }
+                    }; break;
+                case 2: {
+                        x = a - b;
+                        c = son.Next(2);
+                        if (c == 1)
+                        {
+                            z = son.Next(1, 100); t1 = true;
+                            label1.Text = a.ToString() + " - " + b.ToString() + " = " + z.ToString();
+                        }
+                        else 
+                        {
+                            t = true;
+                            label1.Text = a.ToString() + " - " + b.ToString() + " = " + x.ToString();
+                        }
+                    }; break;
+                case 3: {
+                        x = a / b;
+                        c = son.Next(2);
+                        if (c == 1)
+                        {
+                            z = son.Next(1, 100); t1 = true;
+                            label1.Text = a.ToString() + " / " + b.ToString() + " = " + z.ToString();
+                        }
+                        else 
+                        {
+                             t = true;
+                            label1.Text = a.ToString() + " / " + b.ToString() + " = " + x.ToString();
+                        }
+                    }; break;
+                case 4: {
+                        x = a * b;
+                        c = son.Next(2);
+                        if (c == 1)
+                        {
+                            z = son.Next(1, 100); t1 = true;
+                            label1.Text = a.ToString() + " * " + b.ToString() + " = " + z.ToString();
+                        }
+                        else 
+                        {
+                              t = true;
+                            label1.Text = a.ToString() + " * " + b.ToString() + " = " + x.ToString();
+                        }
+                      
+                    }; break;
+            }
+        }
+    }
+}
